70fb228aa50155e2d4bac5c7c24fa436cd71e359
khalidamairi3 <khalidamairi3@gmail.com>
2020-10-29 22:50:33 -0600

    custom exception

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from  subtraction import subtract
 from multiplication import multiply
 from division import divide
 
+class CalculatorInputError(ValueError):
+    pass
 
 
+
+def getUserInput():
+    try:
+        option=input("select the calculation option: ")
+        if option not in ["1","2","3","4"]:
+            raise CalculatorInputError("invalid selection")
+        return option
+    except CalculatorInputError:
+        print("invalid selection")
+        print("1) add  numbers")
+        print("2) subtract numbers")
+        print("3) mutiply numbers")
+        print("4) divide numbers")
+        getUserInput()
 
 print("welcome to simple calculator")
 
@@ -13,17 +29,17 @@ print("2) subtract numbers")
 print("3) mutiply numbers")
 print("4) divide numbers")
 
-option =int( input("choose the calculation option: " ))
+option =getUserInput()
 
-while(option not in [1,2,3,4]):
+# while(option not in [1,2,3,4]):
 
-    print("please enter a number between 1 and 4")
-    print("1) add  numbers")
-    print("2) subtract  numbers")
-    print("3) mutiply  numbers")
-    print("4) divide numbers")    
-    option = int(input("choose the calculation option: " ))
-if option == 3 or option==4:
+#     print("please enter a number between 1 and 4")
+#     print("1) add  numbers")
+#     print("2) subtract  numbers")
+#     print("3) mutiply  numbers")
+#     print("4) divide numbers")    
+#     option = int(input("choose the calculation option: " ))
+if option == '3' or option=='4':
     result =1
 else:
     result=0
@@ -37,20 +53,21 @@ while(choice == True):
         try:
             a=float(a)
             break
-        except:
+        except :
             print("only numbers allowed")
             a= input("enter your  number: ")
-    if(option == 1):
+
+    if(option == '1'):
         result = add(a,result)
-    if(option == 2 and iteration == 0):
+    if(option == '2' and iteration == 0):
         result = subtract(a,result)
-    elif (option == 2):
+    elif (option == '2'):
         result = subtract(result,a)
-    if(option == 3):
+    if(option == '3'):
         result = multiply(a,result)
-    if(option == 4 and iteration == 0):
+    if(option == '4' and iteration == 0):
         result = divide(a,result)
-    elif (option == 4):
+    elif (option == '4'):
         result = divide(result,a)
     iteration = iteration+1
     choice = input("do you want to add one more number to youe operation y/n : ")=="y"
